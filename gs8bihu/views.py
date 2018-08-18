8f0d583e9e8ead219ae5b70406b01aeaacfe5036
steemian-io <rlge@live.com>
2018-08-18 16:22:28 +0800

    只做测试，看看能不能在pycharm中使用git2

--- gs8bihu/views.py
+++ gs8bihu/views.py
@@ -48,7 +48,7 @@ class TestView(APIView):
                              r'[a-zA-Z.]*bihu.com/[^<>]*?)&amp;sa[^<>]*>(?P<title>.*?)</a>\s*</h3>[\s\S]*?'
                              r'<span\s*class="st">(?P<snap>[\s\S]*?)</span>', re.I)
 
-        urllist = [] #测试
+        urllist = [] #测试2
 
         for dict in pattern.finditer(r.text):
             urllist.append(dict.groupdict())
