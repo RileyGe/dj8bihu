8c90d50674c338354b6079d4aa709989cefb30e4
grlsr <grlsr@163.com>
2018-08-19 19:48:11 +0800

    增加推荐用户的后台管理

--- gs8bihu/admin.py
+++ gs8bihu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from gs8bihu.models import RecommendAuthor
+
 
 # Register your models here.
+class RecommendAuthorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'info', 'icon', 'artnum', 'fans', 'order']
+
+
+admin.site.register(RecommendAuthor, RecommendAuthorAdmin)
