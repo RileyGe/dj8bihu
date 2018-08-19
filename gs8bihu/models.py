375b8219b9bd08874034e5ccff8ad88a6a8e0ad2
grlsr <grlsr@163.com>
2018-08-19 19:36:29 +0800

    添加增加用户模型

--- gs8bihu/models.py
+++ gs8bihu/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class RecommendAuthor(models.Model):
+    id = models.IntegerField(u'用户ID', primary_key=True)
+    name = models.CharField(u'用户名', max_length=20)
+    info = models.CharField(u'自我介绍', max_length=100)
+    icon = models.CharField(u'图标', max_length=100)
+    artnum = models.IntegerField(u'文章数量')
+    fans = models.IntegerField(u'粉丝数')
+    order = models.IntegerField(u'排序')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:  # 按时间下降排序
+        ordering = ['-order']
+        verbose_name = "文章"
+        verbose_name_plural = "文章"
